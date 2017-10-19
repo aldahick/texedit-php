64cf5c3d29f5043c218941cc6bcf1e1d59b1a5ba
Alex Hicks <aldahick@iupui.edu>
2017-10-19 10:28:38 -0400

    Update data.sql

--- data.sql
+++ data.sql
@@ -11,9 +11,15 @@ create table if not exists `note` (
     `author_id` int not null,
     `title` varchar(128) not null,
     `body` text not null,
+    `is_shared` tinyint(1) not null default 0,
     `created` datetime default current_timestamp,
 foreign key(`author_id`)
     references `user`(`id`)
     on delete cascade,
 unique key(`author_id`, `title`),
 primary key(`id`));
+
+CREATE PROCEDURE `create_note` (IN `author_id` INT(8), IN `title` VARCHAR(128))
+INSERT INTO `note` (`author_id`, `title`, `body`)
+    VALUES (author_id, title, '')
+    ON DUPLICATE KEY UPDATE `title` = VALUES(`title`);
